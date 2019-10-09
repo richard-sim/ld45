4c5bb662b6b6017e473a4a1e4e854e8cafa2e119
Richard Sim <rsim@devras.com>
2019-10-09 10:15:27 -0700

    Initial commit

---
+++ ld45/Assets/Scripts/GameCoordinator.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCoordinator : MonoBehaviour
+{
+    public static GameCoordinator Instance;
+    
+    private int _level = 0;
+    private int _health = 0;
+    private int _ammo = 0;
+
+    public int Level {
+        get { return _level; }
+    }
+
+    public string LevelName {
+        get { return $"level-{_level:D2}"; }
+    }
+
+    public int Health {
+        get { return _health; }
+        set { _health = value; }
+    }
+
+    public int Ammo {
+        get { return _ammo; }
+        set { _ammo = value; }
+    }
+
+    public void ResetGame() {
+        Debug.Log("Game reset.");
+        
+        _level = 0;
+        _health = 0;
+        _ammo = 0;
+    }
+
+    public void LoadNextLevel() {
+        _level = _level + 1;
+        
+        LevelLoader.Instance.LoadLevel(LevelName);
+    }
+
+    public void LoadLevel(int level) {
+        _level = level;
+        
+        LevelLoader.Instance.LoadLevel(LevelName);
+    }
+
+    private void Awake() {
+        if (Instance != null) {
+            _level = Instance._level;
+            _health = Instance._health;
+            _ammo = Instance._ammo;
+            
+            Destroy(Instance.gameObject);
+            Instance = null;
+        }
+        
+        DontDestroyOnLoad(this.gameObject);
+
+        Instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        ResetGame();
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+}
---
+++ ld45/Assets/Scripts/GameLevelManager.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameLevelManager : MonoBehaviour {
+    public GameObject GameplayUI;
+
+    public GameObject LevelCompleteUI;
+
+    public GameObject GameWinUI;
+
+    public GameObject GameLostUI;
+
+    public GameObject PauseMenu;
+
+    public enum GameState {
+        Playing,
+        PlayPaused,
+        LevelComplete,
+        Won,
+        Lost
+    }
+
+    private GameState _state = GameState.Playing;
+
+    public GameState State {
+        get { return _state; }
+    }
+
+    public void ChangeState(GameState newState) {
+        Debug.Log($"ChangeState({_state} -> {newState})");
+
+        switch (_state) {
+            //case GameState.Intro:
+            //    Debug.Assert(newState == GameState.Playing);
+                
+            //    IntroUI.SetActive(false);
+            //    GameplayUI.SetActive(true);
+                
+            //    break;
+            
+            case GameState.Playing:
+                if (newState == GameState.PlayPaused) {
+                    PauseMenu.SetActive(true);
+                } else if (newState == GameState.LevelComplete) {
+                    LevelCompleteUI.SetActive(true);
+                } else if (newState == GameState.Won) {
+                    GameWinUI.SetActive(true);
+                } else if (newState == GameState.Lost) {
+                    GameLostUI.SetActive(true);
+                }
+                else {
+                    Debug.LogError("Unhandled state: " + newState);
+                }
+
+                break;
+            
+            case GameState.PlayPaused:
+                Debug.Assert(newState == GameState.Playing);
+
+                PauseMenu.SetActive(false);
+
+                break;
+            
+            case GameState.LevelComplete:
+//                Debug.Break();
+                Debug.Log("Level complete!");
+                break;
+            
+            case GameState.Won:
+//                Debug.Break();
+                Debug.Log("Game won!");
+                break;
+            
+            case GameState.Lost:
+//                Debug.Break();
+                Debug.Log("Game lost!");
+                break;
+        }
+
+        _state = newState;
+    }
+
+    // Awake is called when the script instance is being loaded
+    private void Awake() {
+        //IntroUI.SetActive(true);
+        GameplayUI.SetActive(true);
+        LevelCompleteUI.SetActive(false);
+        GameWinUI.SetActive(false);
+        GameLostUI.SetActive(false);
+        PauseMenu.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+}
